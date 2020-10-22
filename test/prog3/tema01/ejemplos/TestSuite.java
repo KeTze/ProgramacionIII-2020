d141321b65dd7cb33cf577770540020ec3fa1251
Ekaitz Polledo <ekaitz.polledo@opendeusto.es>
2020-10-22 16:23:51 +0200

    Test Suite

---
+++ test/prog3/tema01/ejemplos/TestSuite.java
@@ -0,0 +1,11 @@
+package prog3.tema01.ejemplos;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({RestaTest.class, SumaTest.class})
+public class TestSuite {
+
+}
