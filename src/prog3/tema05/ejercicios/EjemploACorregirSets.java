96ecb53c7cde8452b2d5617bddfde343d75f425a
Ekaitz Polledo <ekaitz.polledo@opendeusto.es>
2020-11-26 18:05:36 +0100

    Ejercicio resuelto Sets

--- src/prog3/tema05/ejercicios/EjemploACorregirSets.java
+++ src/prog3/tema05/ejercicios/EjemploACorregirSets.java
@@ -11,6 +11,11 @@ public class EjemploACorregirSets {
 		hS.add( new Fantasma( "opera phantom", 4, 2) );
 		hS.add( new Fantasma( "casper", 1, 2) );
 		hS.add( new Fantasma( "myrtle", 5, 10) );
+		Iterator it = hS.iterator();
+		while(it.hasNext()) {
+			Fantasma f = (Fantasma) it.next();
+			System.out.println(f);
+		}
 		System.out.println( hS );  // ¿Por qué añade dos veces a casper?
 		hSS = new TreeSet<>();
 		hSS.add( new Fantasma( "casper", 1, 2) );
@@ -20,7 +25,7 @@ public class EjemploACorregirSets {
 		System.out.println( hSS ); // ¿Por qué da error en ejecución?
 	}
 	
-	private static class Fantasma {
+	private static class Fantasma implements Comparable {
 		String nombre;
 		int x;
 		int y;
@@ -30,6 +35,39 @@ public class EjemploACorregirSets {
 			this.x = x;
 			this.y = y;
 		}	
+		@Override
+		public String toString() {
+			return nombre + " (" + x + "," + y + ")";
+		}
+		
+		@Override
+		public int hashCode() {
+			/*int result = nombre.hashCode();
+	        result = 31 * result + x;
+	        result = 31 * result + y;
+			 * */
+			return Objects.hash(nombre, x, y);
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			// TODO Auto-generated method stub
+			Fantasma c = (Fantasma) obj;
+			if(nombre.equals(c.nombre) && x == c.x && y == c.y) return true;
+			else return false;
+		}
+		@Override
+		public int compareTo(Object o) {
+			// TODO Auto-generated method stub
+			return nombre.compareTo(((Fantasma)o).nombre);
+			
+			/*
+			Fantasma c = (Fantasma) o;
+			if(y > c.y) return 1;
+			else if(y < c.y) return -1;
+			else return 0;
+			*/
+		}
 	}
 	
 }
